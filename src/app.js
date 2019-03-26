6e5a2e84b8d2f46e16e9224fad8e8ad58e35dbb0
fajardoralf <fajardoralf@gmail.com>
2019-03-26 09:23:20 +0100

    created node

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
-import express from 'express';
-
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
+app.use(express.static(path.join(__dirname + '/public')));
 
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8081);
