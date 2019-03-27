7c053cbb979f69c7fa0c2d43842a6e520b0b7c35
Kenneth Ngo <kenneth.ngo@no.experis.com>
2019-03-27 13:43:27 +0100

    Fixed home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,10 @@
 
 <script>
 import LandingPage from "@/components/LandingPage/LandingPage.vue";
-import Forecast from "@/components/Forecast/Forecast.vue";
 
 export default {
   components: {
-    LandingPage,
-    Forecast
+    LandingPage
   }
 };
 </script>
